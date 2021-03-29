c0a535f8c6d16e7ec7743abf5bcbf59b28942651
Benjamin Bouchet <contact@benjamin-bouchet.com>
2021-03-29 12:28:02 +0200

    Segment plugin only exposes client and disabled

--- plugin-segment/lib/itly/plugin/segment/segment.rb
+++ plugin-segment/lib/itly/plugin/segment/segment.rb
@@ -11,7 +11,7 @@ class Itly
     # Automatically loaded at runtime in any new +Itly+ object
     #
     class Segment < Plugin
-      attr_reader :logger, :client, :write_key, :disabled
+      attr_reader :client, :disabled
 
       ##
       # Instantiate a new Plugin::Segment
@@ -35,10 +35,10 @@ class Itly
         @logger = options.logger
 
         # Log
-        logger&.info "#{plugin_id}: load()"
+        @logger&.info "#{plugin_id}: load()"
 
         if @disabled
-          logger&.info "#{plugin_id}: plugin is disabled!"
+          @logger&.info "#{plugin_id}: plugin is disabled!"
           return
         end
 
@@ -53,7 +53,7 @@ class Itly
         end
 
         @client = ::SimpleSegment::Client.new \
-          write_key: @write_key, logger: logger,
+          write_key: @write_key, logger: @logger,
           on_error: error_handler
       end
 
@@ -69,7 +69,7 @@ class Itly
         return unless enabled?
 
         # Log
-        logger&.info "#{plugin_id}: identify(user_id: #{user_id}, properties: #{properties})"
+        @logger&.info "#{plugin_id}: identify(user_id: #{user_id}, properties: #{properties})"
 
         # Send through the client
         @client.identify user_id: user_id, traits: properties.properties
@@ -88,7 +88,7 @@ class Itly
         return unless enabled?
 
         # Log
-        logger&.info "#{plugin_id}: group(user_id: #{user_id}, group_id: #{group_id}, properties: #{properties})"
+        @logger&.info "#{plugin_id}: group(user_id: #{user_id}, group_id: #{group_id}, properties: #{properties})"
 
         # Send through the client
         @client.group user_id: user_id, group_id: group_id, traits: properties.properties
@@ -106,7 +106,7 @@ class Itly
         return unless enabled?
 
         # Log
-        logger&.info "#{plugin_id}: track(user_id: #{user_id}, event: #{event.name}, properties: #{event.properties})"
+        @logger&.info "#{plugin_id}: track(user_id: #{user_id}, event: #{event.name}, properties: #{event.properties})"
 
         # Send through the client
         @client.track user_id: user_id, event: event.name, properties: event.properties
@@ -126,7 +126,7 @@ class Itly
         return unless enabled?
 
         # Log
-        logger&.info "#{plugin_id}: alias(user_id: #{user_id}, previous_id: #{previous_id})"
+        @logger&.info "#{plugin_id}: alias(user_id: #{user_id}, previous_id: #{previous_id})"
 
         # Send through the client
         @client.alias user_id: user_id, previous_id: previous_id
